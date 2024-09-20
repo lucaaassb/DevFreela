137dced6dc1b693b8f3522c6a7a3d21003238e61
Lucas <lucassb395@gmail.com>
2024-09-19 21:39:31 -0300

    Ajuste de models e controllers

--- DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -1,5 +1,6 @@
 ﻿using DevFreela.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace DevFreela.API.Controllers
 {
@@ -7,6 +8,12 @@ namespace DevFreela.API.Controllers
     [ApiController]
     public class ProjectsController : ControllerBase
     {
+        private readonly FreelanceTotalCostConfig _config;
+        public ProjectsController(IOptions<FreelanceTotalCostConfig> options)
+        {
+            _config = options.Value;
+        }
+
         // GET api/projects?search=crm
         [HttpGet]
         public IActionResult Get(string search)
@@ -25,6 +32,10 @@ namespace DevFreela.API.Controllers
         [HttpPost]
         public IActionResult Post(CreateProjectInputModel model)
         {
+            if(model.TotalCost < _config.Minimum || model.TotalCost > _config.Maximum)
+            {
+                return BadRequest("Número fora dos limites.");
+            }
             return CreatedAtAction(nameof(GetById), new { id = 1 }, model);
         }
 
@@ -43,17 +54,24 @@ namespace DevFreela.API.Controllers
         }
 
         // PUT api/projects/1234/start
-        [HttpPut("{id}")]
+        [HttpPut("{id}/start")]
         public IActionResult Start(int id)
         {
             return NoContent();
         }
 
         // PUT api/projects/1234/complete
-        [HttpPut("{id}")]
+        [HttpPut("{id}/complete")]
         public IActionResult Complete(int id)
         {
             return NoContent();
+        }
+
+        //POST api/projects/1234/comments
+        [HttpPost("{id}/comments")]
+        public IActionResult PostComment(int id, CreateProjectCommentInputModel model)
+        {
+            return Ok();
         }
     }
 }
