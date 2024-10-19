dbd63b136894c94259160630acbb09cd17968ce0
Lucas <lucassb395@gmail.com>
2024-10-19 11:36:29 -0300

    [FEATURE] Add clean architecture

---
+++ DevFreela.Infrastructure/Migrations/20241019143124_RemoveOldFK.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevFreela.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveOldFK : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProjectComments_Users_UserId",
+                table: "ProjectComments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProjectComments_UserId",
+                table: "ProjectComments");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "ProjectComments");
+
+            migrationBuilder.RenameColumn(
+                name: "isDeleted",
+                table: "UserSkills",
+                newName: "IsDeleted");
+
+            migrationBuilder.RenameColumn(
+                name: "isDeleted",
+                table: "Users",
+                newName: "IsDeleted");
+
+            migrationBuilder.RenameColumn(
+                name: "isDeleted",
+                table: "Skills",
+                newName: "IsDeleted");
+
+            migrationBuilder.RenameColumn(
+                name: "isDeleted",
+                table: "Projects",
+                newName: "IsDeleted");
+
+            migrationBuilder.RenameColumn(
+                name: "isDeleted",
+                table: "ProjectComments",
+                newName: "IsDeleted");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectComments_IdUser",
+                table: "ProjectComments",
+                column: "IdUser");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProjectComments_Users_IdUser",
+                table: "ProjectComments",
+                column: "IdUser",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProjectComments_Users_IdUser",
+                table: "ProjectComments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProjectComments_IdUser",
+                table: "ProjectComments");
+
+            migrationBuilder.RenameColumn(
+                name: "IsDeleted",
+                table: "UserSkills",
+                newName: "isDeleted");
+
+            migrationBuilder.RenameColumn(
+                name: "IsDeleted",
+                table: "Users",
+                newName: "isDeleted");
+
+            migrationBuilder.RenameColumn(
+                name: "IsDeleted",
+                table: "Skills",
+                newName: "isDeleted");
+
+            migrationBuilder.RenameColumn(
+                name: "IsDeleted",
+                table: "Projects",
+                newName: "isDeleted");
+
+            migrationBuilder.RenameColumn(
+                name: "IsDeleted",
+                table: "ProjectComments",
+                newName: "isDeleted");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "ProjectComments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectComments_UserId",
+                table: "ProjectComments",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProjectComments_Users_UserId",
+                table: "ProjectComments",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
